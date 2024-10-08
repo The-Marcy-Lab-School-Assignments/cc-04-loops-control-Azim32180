f4611b923cd6e00272c6bba5e7f607f37b823d80
Azim <annayev.azim@gmail.com>
2024-10-08 14:30:47 -0400

    Finished all 6 and bonus challenge as well

--- index.js
+++ index.js
@@ -36,4 +36,39 @@ const betweenFiveAndTwenty = (num) => {
 // console.log(betweenFiveAndTwenty(100));
 
 // 5
-const sumOfThreeOrFive = 
+const sumOfThreeOrFive = () => {
+  let sum = 0;
+  for (let i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+// console.log(sumOfThreeOrFive());
+
+//6
+
+const isAllLowerCase = (str) => {
+  if (str === str.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
+};
+// console.log(isAllLowerCase("hello"));
+// console.log(isAllLowerCase("Seven Eleven has the worst"));
+
+// Bonus
+
+const countMultiplesOfFive = (array) => {
+  let count = 0;
+  for (let i = 1; i <= array.length; i++) {
+    if (i % 5 === 0) {
+      count += 1;
+    }
+  }
+  return count;
+};
+console.log(countMultiplesOfFive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
